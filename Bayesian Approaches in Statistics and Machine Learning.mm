ee0fbda968d627c755d13643b8c5548a57de93cd
Arseny <=>
2018-11-17 21:39:39 -0800

    Initial Commit

---
+++ Bayesian Approaches in Statistics and Machine Learning.mm
@@ -0,0 +1,94 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Bayesian Approaches in Statistics and Machine Learning" FOLDED="false" ID="ID_1051276732" CREATED="1542518909127" MODIFIED="1542519288832" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="4" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Specific Models Useful to Know" POSITION="right" ID="ID_1501789217" CREATED="1542518980638" MODIFIED="1542519218870" HGAP_QUANTITY="41.74999917298558 pt" VSHIFT_QUANTITY="42.749998725950746 pt">
+<edge COLOR="#ff0000"/>
+<node TEXT="Bayesian PCA" FOLDED="true" ID="ID_1122955494" CREATED="1542519002061" MODIFIED="1542519094589" HGAP_QUANTITY="14.749999977648258 pt" VSHIFT_QUANTITY="-19.49999941885473 pt">
+<node TEXT="See Vetrov&apos;s Lecture" ID="ID_1476480160" CREATED="1542519016286" MODIFIED="1542519022652"/>
+</node>
+<node TEXT="Normal - Normal Model" ID="ID_927269818" CREATED="1542519027168" MODIFIED="1542519213110" HGAP_QUANTITY="24.499999687075622 pt" VSHIFT_QUANTITY="-17.999999463558215 pt"/>
+<node TEXT="Hierarchical Model With A normal Prior" ID="ID_578683073" CREATED="1542519097025" MODIFIED="1542519215094" HGAP_QUANTITY="16.999999910593036 pt" VSHIFT_QUANTITY="13.49999959766866 pt"/>
+</node>
+<node TEXT="Selecting Prior Distributions" POSITION="left" ID="ID_76744569" CREATED="1542519120544" MODIFIED="1542519127172">
+<edge COLOR="#0000ff"/>
+<node TEXT="Informative Priors" ID="ID_1652020750" CREATED="1542519131568" MODIFIED="1542519135644"/>
+<node TEXT="Noninformative Priors" ID="ID_1325926973" CREATED="1542519139713" MODIFIED="1542519145636"/>
+<node TEXT="Semi-Informative Priors" ID="ID_1627400567" CREATED="1542519177171" MODIFIED="1542519183087"/>
+<node TEXT="Improper Priors" ID="ID_102738614" CREATED="1542519191619" MODIFIED="1542519196013"/>
+</node>
+<node TEXT="Evaluation" POSITION="right" ID="ID_474999773" CREATED="1542519221825" MODIFIED="1542519283358">
+<edge COLOR="#00ff00"/>
+<node TEXT="Model Checking" ID="ID_1370856024" CREATED="1542519253645" MODIFIED="1542519279110" HGAP_QUANTITY="21.49999977648259 pt" VSHIFT_QUANTITY="22.499999329447768 pt"/>
+<node TEXT="Model Selection" ID="ID_961364610" CREATED="1542519260170" MODIFIED="1542519276599" HGAP_QUANTITY="16.249999932944775 pt" VSHIFT_QUANTITY="17.99999946355821 pt"/>
+</node>
+<node TEXT="Handling Intractable Integrals" POSITION="left" ID="ID_1091421014" CREATED="1542519228117" MODIFIED="1542519305303" HGAP_QUANTITY="-9.249999307096022 pt" VSHIFT_QUANTITY="80.2499976083637 pt">
+<edge COLOR="#ff00ff"/>
+<node TEXT="Deterministic Numerical Methods" ID="ID_1055774720" CREATED="1542519309890" MODIFIED="1542519331238"/>
+<node TEXT="Analytical Approximations" ID="ID_1322868761" CREATED="1542519319935" MODIFIED="1542519337119"/>
+<node TEXT="Simulations" ID="ID_1765982907" CREATED="1542519340188" MODIFIED="1542519349103"/>
+</node>
+</node>
+</map>
